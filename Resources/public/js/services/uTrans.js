46b05d637af6169078a99ca99d994381ded2b5b3
Guillermo Ferrer <guilleferrer@gmail.com>
2013-03-14 10:54:03 +0100

    changes the way to load translations. Removes the translationLoader object

--- Resources/public/js/services/uTrans.js
+++ Resources/public/js/services/uTrans.js
@@ -1,79 +1,53 @@
+angular.module('undf.translations', ['ng']).value('version', '0.1').value('translations').factory('uTrans', function (translations) {
 
-angular.module('undf.translations', ['ng'])
-    .value('translationsLoader', {
-        load : function (translats) {
-            this.translations = translats;
-        },
-        get: function () {
-            return this.translations;
+    if (!angular.isObject(translations)) {
+
+        throw 'uTrans error. If you use the translations module you must set an object of translations. \n' + '\n.run(function(translationsLoader){ ' + '\n\t// Set the translations ' + '\n\ttranslationsLoader.load({ "text %varialbe%" : "Translation"}); ' + '\n});' + '\n\nUse the following format : \n\n' + '\n{\n' + '    "keyToTranslate1" : "Translation1",\n' + '    "keyToTranslate2": "Translation2"\n' + '};';
+    }
+    // Get them from the config
+    var translator = {};
+
+
+    translator.translations = translations;
+    /**
+     * Searches a translation for the given 'text'
+     * in the object of translations
+     *
+     **/
+    translator.trans = function (text, params) {
+
+        // Validate the arguments
+        if (typeof text !== 'string') {
+            throw 'uTrans error. The text to translate should be an string. A "' + (typeof text) + '" was given';
         }
 
-
-    })
-    .factory('uTrans', function(translationsLoader) {
-
-        var translations = translationsLoader.get();
-
-        if(!angular.isObject(translations)){
-
-            throw 'uTrans error. If you use the translations module you must set an object of translations. \n' +
-            '\n.run(function(translationsLoader){ ' +
-            '\n\t// Set the translations ' +
-            '\n\ttranslationsLoader.load({ "text %varialbe%" : "Translation"}); ' +
-            '\n});' +
-            '\n\nUse the following format : \n\n' +
-            '\n{\n' +
-            '    "keyToTranslate1" : "Translation1",\n' +
-            '    "keyToTranslate2": "Translation2"\n' +
-            '};';
-        }
-        // Get them from the config
-        var translator = {};
-
-        /**
-         * Searches a translation for the given 'text'
-         * in the object of translations
-         *
-         **/
-        translator.trans = function (text, params) {
-
-            // Validate the arguments
-            if(typeof text !== 'string'){
-                throw 'uTrans error. The text to translate should be an string. A "' + (typeof text) + '" was given';
-            }
-
-            // Validate the arguments
-            if(params !== undefined && !angular.isObject(params)){
-                throw 'uTrans error . The parameters provided for the translation of "' +
-                text +
-                '" must be an Object and a "' +
-                (typeof params )  +
-                '" has been provided';
-            }
-
-            var translated = translations[text];
-            // No translation found
-            if(translated === undefined) {
-                return '_' + text + '_';
-            }
-
-            // Translation has been found
-            else {
-                // I have options
-                angular.forEach(params, function (val, key) {
-                    // Find key in translated
-                    translated = translated.replace(key,val);
-                });
-
-                return translated;
-            }
+        // Validate the arguments
+        if (params !== undefined && !angular.isObject(params)) {
+            throw 'uTrans error . The parameters provided for the translation of "' + text + '" must be an Object and a "' + (typeof params) + '" has been provided';
         }
 
-        return translator;
-    })
-    .filter('trans',function(uTrans) {
-
-        return function(text, params) {
-            return uTrans.trans(text, params);
+        var translated = translations[text];
+        // No translation found
+        if (translated === undefined) {
+            return '_' + text + '_';
         }
-    });
+
+        // Translation has been found
+        else {
+            // I have options
+            angular.forEach(params, function (val, key) {
+                // Find key in translated
+                translated = translated.replace(new RegExp(key, 'g'), val);
+            });
+
+            return translated;
+        }
+    };
+
+    return translator;
+}).filter('trans', function (uTrans) {
+
+    return function (text, params) {
+        return uTrans.trans(text, params);
+    };
+});
